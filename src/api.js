df8a01f828d704b9a3bb05f7db06575d3ba9b933
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-02-22 21:39:09 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const searchImages = async (term) => {
+   const response = await axios.get('https://api.unsplash.com/search/photos',{
+        headers: {
+            Authorization: 'Client-ID FYg-VnQ_AwCgdgS8JKWvLo3uQvpdt8bWLwOm_nJVV-I'
+        },
+        params: {
+            query: term,
+        },
+    });
+
+    return response.data.result;
+};
+
+export default searchImages;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+function SearchBar({onSubmit}){
+    const handleClick = () => {
+        onSubmit('cars');
+    };
+    return (
+      <div>
+        <input/>
+        <button onClick={handleClick}>Click me</button>
+      </div>
+    );
+}
+
+export default SearchBar;
