91c2e54919f71de0b5835621675ec00eb4f25991
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-02-25 17:38:15 +0100

    search commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+import searchImages from './api';
 
-function App(){
-    const handleSubmit = (term) => {
-        console.log('Do a search with', term);
+function App() {
+    const handleSubmit = async (term) => {
+      const result = await searchImages(term);
+
+        console.log(result);
     };
     return (
         <div>
             <SearchBar onSubmit={handleSubmit}/>
+            <ImageList/>
         </div>
     );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 function SearchBar({onSubmit}){
-    const handleClick = () => {
-        onSubmit('cars');
+    const [term, setTerm] = useState('');
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        
+        onSubmit(term);
     };
+
+    const handleChange = (event) => {
+        setTerm(event.target.value);
+    };
+
     return (
       <div>
-        <input/>
-        <button onClick={handleClick}>Click me</button>
+        <form onSubmit={handleFormSubmit}>
+            <input value={term} onChange={handleChange} />
+        </form>
       </div>
     );
 }
